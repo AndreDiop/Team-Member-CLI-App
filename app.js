5d3f1a1801b44378debb67a1a14df59be7926b3c
Andre Diop <itsjustandre@gmail.com>
2021-01-14 20:22:35 -0500

    Generated file now appears
    - Team.html is generated in the dir

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { type } = require("os");
+const { breakStatement } = require("@babel/types");
+const { Console } = require("console");
 
 const employees = [];
 
@@ -20,27 +23,139 @@ const addEmployee = [
     type: "list",
     message: "Which team members would you like to add?",
     name: "addEmployee",
-    choices: [
-      { name: "none" },
-      { name: "Intern" },
-      { name: "Engineer" },
-      { name: "Manager" },
-    ],
+    choices: [{ name: "none" }, { name: "Intern" }, { name: "Engineer" }],
   },
 ];
-function init() {
-  console.log("Enter information to create your team.");
+function addToTeam() {
+  console.log("Which Team member would you like to add?");
   inquirer.prompt(addEmployee).then((response) => {
-    if (response.addEmployee === "Engineer") {
-      console.log("Is that so now");
-    } else {
-      console.log(response);
+    // // SWITCH FUNCTION  -->
+    switch (response.addEmployee) {
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+      default:
+        // Call to build our team
+        console.log("Your team has been created");
+        buildTeam();
     }
-
     // Create a function to write README file
   });
 }
-init();
+
+const managerQuestions = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your ID number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your Office Number?",
+    name: "officeNumber",
+  },
+];
+const addManager = () => {
+  inquirer.prompt(managerQuestions).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.email,
+      response.id,
+      response.officeNumber
+    );
+    employees.push(manager);
+    addToTeam();
+  });
+};
+
+const internQuestions = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your ID number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your is your school/university name?",
+    name: "school",
+  },
+];
+const addIntern = () => {
+  inquirer.prompt(internQuestions).then((response) => {
+    const intern = new Intern(
+      response.name,
+      response.email,
+      response.id,
+      response.school
+    );
+    employees.push(intern);
+    addToTeam();
+  });
+};
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your ID number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is your Github user ID?",
+    name: "github",
+  },
+];
+const addEngineer = () => {
+  inquirer.prompt(engineerQuestions).then((response) => {
+    const engineer = new Engineer(
+      response.name,
+      response.email,
+      response.id,
+      response.github
+    );
+    employees.push(engineer);
+    addToTeam();
+  });
+};
+console.log("Enter information about the manager:");
+addManager();
+// Create a function to write README file
+function buildTeam() {
+  fs.writeFileSync("employee.html", render(employees), "utf8");
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
